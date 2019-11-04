24c85cafc5dd28ae9b880f5e1eb17948d56fa360
Wendell <wendell@Wendells-Mac-mini.local>
2019-11-04 15:25:30 +0800

    new test for connected component

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { findByTestAtrr, testStore } from '../utils/test';
 
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+const setup = (initialState = {}) => {
+  const store = testStore(initialState);
+  const wrapper = shallow(<App store={store} />)
+    .childAt(0)
+    .dive();
+  return wrapper;
+};
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    div,
-  );
-  ReactDOM.unmountComponentAtNode(div);
+describe('App Component', () => {
+  let wrapper;
+  beforeEach(() => {
+    const initialState = {
+      posts: [
+        { title: 'title 1', body: 'sample body' },
+        { title: 'title 2', body: 'sample body' },
+        { title: 'title 3', body: 'sample body' },
+      ],
+    };
+    wrapper = setup(initialState);
+  });
+
+  it('should render without errors ', () => {
+    const appComponent = findByTestAtrr(wrapper, 'appComponent');
+    expect(appComponent.length).toBe(1);
+  });
 });
--- src/components/posts/posts.test.js
+++ src/components/posts/posts.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAtrr, checkProps } from '../../../utils/test';
+import { findByTestAtrr, checkProps, testStore } from '../../../utils/test';
 import PostItem from './PostItem';
+import PostsList from './PostsList';
 
 describe('PostItem Component', () => {
   const expectedProps = {
@@ -51,5 +52,32 @@ describe('PostItem Component', () => {
       const component = findByTestAtrr(wrapper, 'postItemComponent');
       expect(component.length).toBe(0);
     });
+  });
+});
+
+const setup = (initialState = {}) => {
+  const store = testStore(initialState);
+  const wrapper = shallow(<PostsList store={store} />)
+    .childAt(0)
+    .dive();
+  return wrapper;
+};
+
+describe('PostList Component', () => {
+  let wrapper;
+  beforeEach(() => {
+    const initialState = {
+      posts: [
+        { title: 'title 1', body: 'sample body' },
+        { title: 'title 2', body: 'sample body' },
+        { title: 'title 3', body: 'sample body' },
+      ],
+    };
+    wrapper = setup(initialState);
+  });
+
+  it('should render without errors ', () => {
+    const component = findByTestAtrr(wrapper, 'postsListComponent');
+    expect(component.length).toBe(1);
   });
 });
